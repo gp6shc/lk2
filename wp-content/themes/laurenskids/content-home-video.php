1f21c7991258b1bd0e34979d1f1cc5171a98bce8
Brian <briano@sachsmedia.com>
2014-09-26 17:20:12 -0400

    Top video area placeholder img

--- wp-content/themes/laurenskids/content-home-video.php
+++ wp-content/themes/laurenskids/content-home-video.php
@@ -5,5 +5,4 @@
 ?>
 
 <div class="video">
-video
 </div>
--- wp-content/themes/laurenskids/home.php
+++ wp-content/themes/laurenskids/home.php
@@ -1,17 +1,19 @@
 <?php
 /*
 Template Name: Home
+*
+* separating out 
 */
 get_header(); ?>
 
 
-<?php get_template_part( 'content', 'home-video' ); //'content-home-video.php' ?>
-		
-<?php get_template_part( 'content', 'home-welcome' ); //'content-home-welcome.php' ?>
+	<?php get_template_part( 'content', 'home-video' ); //'content-home-video.php' ?>
 			
-<?php get_template_part( 'content', 'home-news' ); //'content-home-news.php' ?>
-			
-<?php get_template_part( 'content', 'home-partners' ); //'content-home-partners.php' ?>
+	<?php get_template_part( 'content', 'home-welcome' ); //'content-home-welcome.php' ?>
+				
+	<?php get_template_part( 'content', 'home-news' ); //'content-home-news.php' ?>
+				
+	<?php get_template_part( 'content', 'home-partners' ); //'content-home-partners.php' ?>
 		
 		
 <?php get_footer(); ?>
