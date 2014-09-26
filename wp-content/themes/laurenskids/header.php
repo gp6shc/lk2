d3b21bdf37a2df269e6c5a1053855e01a351c92b
Brian <briano@sachsmedia.com>
2014-09-26 09:16:46 -0400

    initial / asset organization / header + nav

---
+++ wp-content/themes/laurenskids/header.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package laurenskids
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+
+	<header id="masthead" role="banner">
+
+		<div class="header">
+			<img class="logo" src="<?php bloginfo('stylesheet_directory') ?>/img/logo-full.png"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a></img>
+			
+			<div class="top-area">
+				<ul class="social">
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+					<a href="#"><li><i class="fa fa-rss"></i></li></a>
+					<a href="#"><li><i class="fa fa-youtube"></i></li></a>
+					<a href="#"><button class="help">Need Help?</button></a>
+					<a href="#"><button class="join">Join the Movement &amp; Donate</button></a>
+				</ul>
+			</div>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Menu', 'laurenskids' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- /#site-navigation -->
+		</div><!--/.header -->
+
+	</header><!--/#masthead -->
+
+	<div id="content" class="container">
