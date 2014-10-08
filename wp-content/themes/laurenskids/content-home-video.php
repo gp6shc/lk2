bd172cd6082d0f1317b50d5fbb750bd8f842806b
Brian <briano@sachsmedia.com>
2014-10-08 09:57:45 -0400

    full screen video section

--- wp-content/themes/laurenskids/content-home-video.php
+++ wp-content/themes/laurenskids/content-home-video.php
@@ -4,6 +4,6 @@
  */
 ?>
 
-
-<div class="video">
+<div class="video wrapper">
+<iframe width="100%" height="100%" src="//www.youtube.com/embed/9fmb_Cemszo?autoplay=1&autohide=0&showinfo=0&controls=0&rel=0" frameborder="0" allowfullscreen></iframe>
 </div>
