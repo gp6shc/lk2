7f5ac3fddb36836869cdebf5d470f8672e97eb74
Mike <mike@sachsmedia.com>
2014-10-24 18:55:25 -0400

    return to general searchform for get_searchform()

--- wp-content/themes/laurenskids/searchform.php
+++ wp-content/themes/laurenskids/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<i id="js-search-btn" class="fa fa-search"></i>
-	<span id="js-search-contain" class="search-elements display-none">
+	<i class="fa fa-search"></i>
+	<span>
 		<input type="search" class="search-field" placeholder="Search for..." value="<?php echo get_search_query() ?>" name="s" title="Search" />
 		<input type="submit" class="search-submit" value="Search" />
 	</span>
