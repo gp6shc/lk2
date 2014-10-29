457488f3dc7eb8452dbe633d0e19bbf8a761ada6
Mike <mike@sachsmedia.com>
2014-10-29 19:39:57 -0400

    interior page style and build out

--- wp-content/themes/laurenskids/template-image-bg-nav.php
+++ wp-content/themes/laurenskids/template-image-bg-nav.php
@@ -26,7 +26,7 @@ get_header(); ?>
 	
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					    <header class="entry-header">
-					    	<?php the_title( '<h2 class="centered-text butterfly">', '</h2>' ); ?>
+					    	<h2 class="centered-text butterfly"><?php the_title() ?></h2>
 					    </header><!-- .entry-header -->
 					
 					    <div class="entry-content centered-text">
--- wp-content/themes/laurenskids/template-image-bg.php
+++ wp-content/themes/laurenskids/template-image-bg.php
@@ -16,10 +16,6 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 	
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					    <header class="entry-header">
-					    	<?php the_title( '<h2 class="centered-text butterfly">', '</h2>' ); ?>
-					    </header><!-- .entry-header -->
-					
 					    <div class="entry-content centered-text">
 					    	<?php the_content(); ?>
 					    </div><!-- .entry-content -->
