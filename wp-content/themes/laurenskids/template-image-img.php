cb1279f6c8aa1eba649e5b31f8ae3791e0141bdf
Mike <mike@sachsmedia.com>
2014-11-07 16:53:51 -0500

    renamed templates

--- wp-content/themes/laurenskids/template-image-img.php
+++ wp-content/themes/laurenskids/template-image-img.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Image Background + Submenu
+ * Template Name: Full Image
  *
  * @package laurenskids
  */
@@ -8,16 +8,7 @@
 get_header(); ?>
 
 
-<div class="background-image" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>);">
-	<div class="subnav">
-		<div class="container">
-			<?php
-			if( is_page( array(8,75,77,79,81) ) ) { // About page and its children
-				wp_nav_menu( array( 'theme_location' => 'about' ) );
-			}?>
-		</div>
-	</div>
-	
+<div class="background-image" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>);padding-top: 72px;">	
 	<div class="container translucent">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
@@ -25,14 +16,9 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 	
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					    <header class="entry-header">
-					    	<h2 class="centered-text butterfly"><?php the_title() ?></h2>
-					    </header><!-- .entry-header -->
-					
 					    <div class="entry-content centered-text">
 					    	<?php the_content(); ?>
 					    </div><!-- .entry-content -->
-					    
 					
 					    <footer class="entry-footer">
 					    	<?php edit_post_link( __( 'Edit', 'laurenskids' ), '<span class="edit-link">', '</span>' ); ?>
